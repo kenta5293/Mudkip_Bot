c5beb172f32effdffc9260a43e4530d5346acd4d
kenta5293 <kenta.03.1110@gmail.com>
2023-07-14 19:12:44 +0900

    ✨ main.js 생성 및 봇 작동 코드 추가

---
+++ src/main.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.once("ready", (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "sushi") {
+    message.reply("🍣");
+  }
+});
+
+client.login(process.env.TOKEN);
